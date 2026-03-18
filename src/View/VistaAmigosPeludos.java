a791f6e11ebfbe1f61bab4686501edbed67541d0
JavierRegalado <ikeam@plaiaundi.net>
2026-03-18 14:02:47 +0100

    metodoView

--- src/View/VistaAmigosPeludos.java
+++ src/View/VistaAmigosPeludos.java
@@ -2,4 +2,11 @@ package View;
 
 public class VistaAmigosPeludos {
 
+    public static void mostrarMenuGestion() {
+        System.out.println("\n--- GESTIÓN DEL ADMINISTRADOR ---");
+        System.out.println("1. Gestionar animales");
+        System.out.println("2. Gestionar centros");
+        System.out.println("3. Gestionar adopciones");
+        System.out.println("4. Salir");
+    }
 }
